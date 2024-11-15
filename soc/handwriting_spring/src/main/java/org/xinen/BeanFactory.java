9e3c0f5e3337882b53d824a56401508cd4cf0892
xinen <xien00@outlook.com>
2024-11-15 17:27:06 +0800

    creat source code

---
+++ soc/handwriting_spring/src/main/java/org/xinen/BeanFactory.java
@@ -0,0 +1,27 @@
+package org.xinen;
+
+/*
+ * BeanFactory 生成和使用Bean对象的工厂，将Bean对象的定义存储到Map中以便获取
+ */
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class BeanFactory {
+
+    private Map<String,BeanDefinition> beanDefinitionMap = new ConcurrentHashMap<>();
+
+    /*
+     * 根据Bean的名称获取Bean对象
+     */
+    public Object getBean(String name){
+        return beanDefinitionMap.get(name).getBean();
+    }
+
+    /*
+     * 注册BeanDefinition 对象
+     */
+    public void registerBeanDefinition(String name, BeanDefinition beanDefinition){
+        beanDefinitionMap.put(name,beanDefinition);
+    }
+}
